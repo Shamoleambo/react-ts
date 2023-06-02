4401c3ecc014874d24276c76163388fda1c08390
Tiago Gomes <tidgomes@gmail.com>
2023-06-02 11:27:46 -0300

    create a Todo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import './App.css'
+import Todos from './components/Todos'
 
 function App() {
-  return <h1>Hello React + TS</h1>
+  return (
+    <>
+      <Todos />
+    </>
+  )
 }
 
 export default App
---
+++ src/components/Todos.tsx
@@ -0,0 +1,8 @@
+export default function Todos() {
+  return (
+    <ul>
+      <li>Lose weight</li>
+      <li>Learn to be more temperant</li>
+    </ul>
+  )
+}
