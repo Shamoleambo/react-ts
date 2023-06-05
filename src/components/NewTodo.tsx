ddf50229ff72eb9d8026e0d95467befd160703bb
Tiago Gomes <tidgomes@gmail.com>
2023-06-05 13:53:06 -0300

    apply useRef hook to capture the value of the text input

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,12 +1,19 @@
+import { useRef } from 'react'
+
 const NewTodo = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null)
   const submitHandler = (event: React.FormEvent): void => {
     event.preventDefault()
+
+    const enteredText = todoTextInputRef.current!.value
+
+    if (enteredText.trim().length === 0) return
   }
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor='text'>Todo text</label>
-      <input id='text' type='text' />
+      <input id='text' type='text' ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
   )
