f3bc3596cbfb6df3e225710585265ea4f76ac920
Tiago Gomes <tidgomes@gmail.com>
2023-06-05 14:09:21 -0300

    make App a controlled component for the todos state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { useState } from 'react'
+import NewTodo from './components/NewTodo'
 import Todos from './components/Todos'
 import Todo from './models/todo'
 
 function App() {
-  const todos = [
-    new Todo('Learn to Fight'),
-    new Todo('Learn to survive in a forest')
-  ]
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const addTodoHandler = (todoText: string): void => {
+    const newTodo = new Todo(todoText)
+    setTodos((prevState) => [...prevState, newTodo])
+  }
 
   return (
     <>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </>
   )
