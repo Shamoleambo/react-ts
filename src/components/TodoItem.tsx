17952922a41bf22832d38e892e43404c50835c36
Tiago Gomes <tidgomes@gmail.com>
2023-06-06 10:21:50 -0300

    prop drill the removeTodoHandler to the list element

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
-import Todo from '../models/todo'
 import classes from './TodoItem.module.css'
 
-const TodoItem: React.FC<{ todo: Todo }> = (props) => {
-  return <li className={classes.item}>{props.todo.text}</li>
+const TodoItem: React.FC<{
+  todoText: string
+  todoId: string
+  onRemoveTodo: (event: React.MouseEvent) => void
+}> = (props) => {
+  return (
+    <li id={props.todoId} className={classes.item} onClick={props.onRemoveTodo}>
+      {props.todoText}
+    </li>
+  )
 }
 
 export default TodoItem
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -3,11 +3,19 @@ import TodoItem from './TodoItem'
 import Todo from '../models/todo'
 import classes from './Todos.module.css'
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{
+  items: Todo[]
+  onRemoveTodo: (event: React.MouseEvent) => void
+}> = (props) => {
   return (
     <ul className={classes.todos}>
       {props.items.map((item) => (
-        <TodoItem key={item.id} todo={item} />
+        <TodoItem
+          key={item.id}
+          todoText={item.text}
+          todoId={item.id}
+          onRemoveTodo={props.onRemoveTodo}
+        />
       ))}
     </ul>
   )
